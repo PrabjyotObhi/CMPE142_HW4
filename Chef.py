054c1bc64a5ee8c76d1ebd9e23a9518c99156702
PrabjyotObhi <46588997+PrabjyotObhi@users.noreply.github.com>
2019-05-21 13:10:37 -0700

    Update Chef.py

--- Chef.py
+++ Chef.py
@@ -29,6 +29,7 @@ class ChefThread(Thread):
             time.sleep(random.random())
 
 
+
 class CustomerThread(Thread):  # waiting for fries[2] and sodas[0], has unlimited burgers
 
     food = ['soda', 'burger', 'fries']
@@ -49,3 +50,61 @@ class CustomerThread(Thread):  # waiting for fries[2] and sodas[0], has unlimite
             condition.notify()
             condition.release()
             time.sleep(random.random())
+
+
+class Customer2Thread(Thread): #waiting for burgers[1] and sodas[0]
+
+    food = ['soda', 'burger', 'fries']
+
+    def run(self):
+        global queue
+        while True:
+            condition.acquire()
+            if not queue:
+                print("Nothing in queue, consumer is waiting")
+                condition.wait()
+                print("Producer added something to queue and notified the consumer")
+            if queue and queue[0] is self.food[2]:
+                print("Customer 2 already has unlimited supply of ", self.food[2])
+                condition.wait()
+            item = queue.pop(0)
+            print("Customer 2 consumed", item)
+            condition.notify()
+            condition.release()
+            time.sleep(random.random())
+
+class Customer3Thread(Thread): #waiting for burger[1] and fries[2]
+
+    food = ['soda', 'burger', 'fries']
+
+    def run(self):
+        global queue
+        while True:
+            condition.acquire()
+            if not queue:
+                print("Nothing in queue, consumer is waiting")
+                condition.wait()
+                print("Producer added something to queue and notified the consumer")
+            if queue and queue[0] is self.food[0]:
+                print("Customer 3 already has unlimited supply of ", self.food[0])
+                condition.wait()
+            item = queue.pop(0)
+            print("Customer 3 consumed", item)
+            condition.notify()
+            condition.release()
+            time.sleep(random.random())
+
+for val in range(100):
+    ChefThread().start()
+    #if the queue isnt empty
+    if queue:
+        if queue[val%10] is queue[val+1]%10:
+            continue
+        if (queue[val%10] is 1 and queue[val+1%10] is 2) or (queue[val%10] is 2 and queue[val+1%10] is 1):
+            Customer2Thread().start()
+        if (queue[val%10] is 1 and queue[val+1%10] is 3) or (queue[val%10] is 3 and queue[val+1%10] is 2):
+            CustomerThread().start()
+        if (queue[val%10] is 2 and queue[val+1%10] is 3) or (queue[val%10] is 3 and queue[val+1%10] is 2):
+            Customer3Thread().start()
+    else:
+        print("No one can eat because the queue is empty")
