9a3d22ed7ccc5bd9f05dab43e45a277ae6a65367
PrabjyotObhi <46588997+PrabjyotObhi@users.noreply.github.com>
2019-05-20 22:56:04 -0700

    Update Chef.py

--- Chef.py
+++ Chef.py
@@ -29,4 +29,23 @@ class ChefThread(Thread):
             time.sleep(random.random())
 
 
+class CustomerThread(Thread):  # waiting for fries[2] and sodas[0], has unlimited burgers
 
+    food = ['soda', 'burger', 'fries']
+
+    def run(self):
+        global queue
+        while True:
+            condition.acquire()
+            if not queue:
+                print("Nothing in queue, consumer is waiting")
+                condition.wait()
+                print("Producer added something to queue and notified the consumer")
+            '''if queue and queue[0] is self.food[1]:
+                print("Customer 1 already has unlimited supply of ", self.food[1])
+                condition.wait()'''
+            item = queue.pop(0)
+            print("Customer 1 consumed", item)
+            condition.notify()
+            condition.release()
+            time.sleep(random.random())
